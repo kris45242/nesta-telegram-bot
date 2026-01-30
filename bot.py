d9312c9359d3198a37a89ddef7574c86fa609283
kris45242 <kris45242@gmail.com>
2026-01-30 15:15:23 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,7 +6,8 @@ from aiogram.utils.markdown import hbold
 import asyncio
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
-MANAGER_CHAT_ID = int(os.getenv("MANAGER_CHAT_ID"))
+MANAGER_CHAT_ID = int(os.getenv("MANAGER_CHAT_ID = 0
+"))
 
 bot = Bot(token=BOT_TOKEN)
 dp = Dispatcher()
